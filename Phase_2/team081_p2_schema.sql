abbe92abe148b7c5dc919d1ace3336397fb64795
zkour3 <zkour3@gatech.edu>
2021-10-31 21:11:02 -0700

    Create team081_p2_schema.sql

---
+++ Phase_2/team081_p2_schema.sql
@@ -0,0 +1,151 @@
+
+--********************************************************************
+--Phase 2 Schema  | CS 6400 – Fall 2021 | Team 081
+--********************************************************************
+
+CREATE TABLE   EmployeeUser (
+    Username  VARCHAR(50) NOT NULL,
+    Password  VARCHAR(60) NOT NULL,
+    First_name  VARCHAR(100) NOT NULL,
+	Last_name	VARCHAR(100) NOT NULL,
+    Job_type  VARCHAR (100) NOT NULL,
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Manufacturer (
+Manufacturer_name VARCHAR(50) NOT NULL,
+PRIMARY KEY(Manufacturer_name)
+);
+
+CREATE TABLE Customer (
+Customer_id INTEGER IDENTITY(1,1) PRIMARY KEY, -- Artificial Identifier for UNION
+Phone_number NUMERIC(10,0) NOT NULL,
+Email VARCHAR(50) NULL,
+Street_address VARCHAR(100) NOT NULL,
+City VARCHAR(50) NOT NULL,
+State VARCHAR(50) NOT NULL,
+Postal_code INTEGER NOT NULL
+);
+
+CREATE TABLE Vehicle (
+VIN VARCHAR (50) NOT NULL,
+Year INTEGER NOT NULL,
+Model_name VARCHAR(50) NOT NULL,
+Description VARCHAR(250) NULL,
+List_price FLOAT(24) NOT NULL,
+Invoice_price FLOAT(24) NOT NULL,
+Inventory_date DATETIME NOT NULL,
+Manufacturer_name VARCHAR(50) NOT NULL,
+Username  VARCHAR (50) NOT NULL,
+PRIMARY KEY(VIN),
+FOREIGN KEY (Manufacturer_name) REFERENCES Manufacturer(Manufacturer_name),
+FOREIGN KEY (Username) REFERENCES EmployeeUser(Username)
+);
+
+CREATE TABLE Color (
+VIN VARCHAR (50) NOT NULL,
+Color  VARCHAR(50) NOT NULL,
+PRIMARY KEY(VIN,Color),
+FOREIGN KEY(VIN) REFERENCES Vehicle (VIN)
+);
+
+CREATE TABLE Car (
+VIN VARCHAR (50) NOT NULL,
+Doors_count INT NOT NULL,
+PRIMARY KEY(VIN),
+FOREIGN KEY(VIN) REFERENCES Vehicle (VIN)
+);
+
+CREATE TABLE Convertible (
+VIN VARCHAR(50) NOT NULL,
+Roof_type VARCHAR(50) NOT NULL,
+Back_seat_count INT NOT NULL,
+PRIMARY KEY(VIN),
+FOREIGN KEY(VIN) REFERENCES Vehicle (VIN)
+);
+
+CREATE TABLE Truck (
+VIN VARCHAR (50) NOT NULL,
+Cargo_capacity FLOAT NOT NULL,
+Cargo_cover_type VARCHAR(50) NULL,
+Axle_count INT NOT NULL,
+PRIMARY KEY (VIN),
+FOREIGN KEY(VIN) REFERENCES Vehicle (VIN)
+);
+
+CREATE TABLE VanMinivan (
+VIN VARCHAR(50) NOT NULL,
+Has_driver_back_door BIT NOT NULL, --1 FOR yes 0 for NO
+PRIMARY KEY(VIN),
+FOREIGN KEY(VIN) REFERENCES Vehicle (VIN)
+);
+
+CREATE TABLE SUV (
+VIN VARCHAR (50) NOT NULL,
+Drivetrain_type VARCHAR(50) NOT NULL,
+Cupholder_count INT NOT NULL,
+PRIMARY KEY (VIN),
+FOREIGN KEY(VIN) REFERENCES Vehicle (VIN)
+);
+
+CREATE TABLE Person(
+Driver_license INTEGER NOT NULL,
+Customer_id INTEGER NOT NULL, 
+First_name VARCHAR(50) NOT NULL,
+Last_name VARCHAR(50) NOT NULL,
+PRIMARY KEY (Driver_license),
+FOREIGN KEY(Customer_id) REFERENCES Customer(Customer_id)
+);
+
+
+CREATE TABLE Business(
+TIN INTEGER NOT NULL,
+Customer_id INTEGER NOT NULL,
+Contact_name VARCHAR(50) NOT NULL, 
+Contact_title VARCHAR(50) NOT NULL,
+Business_name VARCHAR(50) NOT NULL,
+PRIMARY KEY (TIN),
+FOREIGN KEY(Customer_id) REFERENCES Customer(Customer_id)
+);
+
+
+CREATE TABLE Sale(
+VIN VARCHAR (50) NOT NULL,
+Username  VARCHAR (50) NOT NULL,
+Customer_id INTEGER NOT NULL,
+Sale_price FLOAT NOT NULL,
+Sale_date DATETIME NOT NULL,
+PRIMARY KEY (VIN,Username,Customer_id),
+FOREIGN KEY(VIN) REFERENCES Vehicle(VIN),
+FOREIGN KEY(Username) REFERENCES EmployeeUser(Username),
+FOREIGN KEY(Customer_id) REFERENCES Customer(Customer_id)
+);
+
+
+CREATE TABLE Repair (
+VIN VARCHAR (50) NOT NULL,
+Customer_id INTEGER NOT NULL,
+Start_date DATETIME NOT NULL,
+Labor_charges FLOAT NULL,
+Total_cost FLOAT NULL,
+Description VARCHAR(500) NOT NULL,
+Completion_date DATETIME NULL,
+Odometer_reading INTEGER NOT NULL,
+Username VARCHAR(50) NOT NULL,
+PRIMARY KEY(VIN,Start_date,Customer_id),
+FOREIGN KEY(VIN) REFERENCES Vehicle (VIN),
+FOREIGN KEY(Customer_id) REFERENCES Customer(Customer_id),
+CHECK(Completion_date>=Start_date)
+);
+
+CREATE TABLE Part(
+VIN VARCHAR (50) NOT NULL,
+Customer_id INT NOT NULL,
+Start_date DATETIME NOT NULL,
+Part_number INT NOT NULL,
+Vendor_name VARCHAR(100) NULL,
+Quantity INT NOT NULL,
+Price FLOAT NOT NULL,
+PRIMARY KEY(VIN,Customer_id,Start_date,Part_number),
+FOREIGN KEY(VIN,Start_date,Customer_id) REFERENCES Repair(VIN,Start_date,Customer_id)
+);
