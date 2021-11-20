e0e3a4c6eaa5e34f06c04202bc977c748870bde7
Mauricio Piraquive <71890811+empira8840@users.noreply.github.com>
2021-11-20 15:00:52 -0500

    Mauricio Local update

--- Phase_3/Jaunty2/mainlanding/forms.py
+++ Phase_3/Jaunty2/mainlanding/forms.py
@@ -18,51 +18,48 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput())
 
 
-
 class QueryVehicleForm(forms.Form):
-   '''
+    '''
    TODO: right now color chices are hard coded
    we want to retrieve these with a query pass as parameter
    TODO: Vehicle type, manufacturer name, and model year, keyword is a drop down
    '''
-   color_choices = get_colors()
+    color_choices = get_colors()
 
-   vehicle_type = forms.CharField()
-   manufacturer_name = forms.CharField()
-   model_year = forms.DateField()
-   color = forms.ChoiceField(choices=color_choices)
-   list_price = forms.FloatField()
-   keywords = forms.CharField()
-
-
+    vehicle_type = forms.CharField()
+    manufacturer_name = forms.CharField()
+    model_year = forms.DateField()
+    color = forms.ChoiceField(choices=color_choices)
+    list_price = forms.FloatField()
+    keywords = forms.CharField()
 
 
 class ReportTypes(forms.Form):
-   report_choices = ((1, "Sales by Color"), (2, "Sales by Type"), (3, "Sales by Manufacturer"),
-                     (4, "Gross Customer Income"), (5, "Average Time in Inventory"), (6, "Part Statistics"),
-                     (7, "Below Cost Sales"), (8, "Repairs By Manufacturer/Type/Model"), (9, "Monthly Sales"),)
-   reports = forms.ChoiceField(choices=report_choices)
+    report_choices = ((1, "Sales by Color"), (2, "Sales by Type"), (3, "Sales by Manufacturer"),
+                      (4, "Gross Customer Income"), (5, "Average Time in Inventory"), (6, "Part Statistics"),
+                      (7, "Below Cost Sales"), (8, "Repairs By Manufacturer/Type/Model"), (9, "Monthly Sales"),)
+    reports = forms.ChoiceField(choices=report_choices)
 
 
 class FilterBy(forms.Form):
-   filter_choices = ((1, "Sold Vehicles"), (2, "Unsold Vehicles"), (3, "All Vehicles"),)
-   filter = forms.ChoiceField(choices=filter_choices)
+    filter_choices = ((1, "Sold Vehicles"), (2, "Unsold Vehicles"), (3, "All Vehicles"),)
+    filter = forms.ChoiceField(choices=filter_choices)
 
 
 class LookupCustomer(forms.Form):
-   drivers_licens_nr = forms.IntegerField()
-   tin = forms.IntegerField()
+    drivers_licens_nr = forms.IntegerField()
+    tin = forms.IntegerField()
 
 
 class AddCustomer(forms.Form):
-  phone_number = forms.CharField()
-  email = forms.EmailField(required = False,)
-  street_address = forms.CharField()
-  city = forms.CharField()
-  state = forms.CharField()
-  postal_code = forms.CharField()
+    phone_number = forms.CharField()
+    email = forms.EmailField(required=False, )
+    street_address = forms.CharField()
+    city = forms.CharField()
+    state = forms.CharField()
+    postal_code = forms.CharField()
 
-  def clean_data(self):
+    def clean_data(self):
         pass
 
 
